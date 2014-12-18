37a0e3e24ba59ab099b57c0d284f028b6b7b0cfa
jpann <ikonmobile@gmail.com>
2014-12-18 08:35:17 -0700

    Set default to contains searching.

--- VSTARegistrationClear/VSTARegistrationClear/Properties/Settings.Designer.cs
+++ VSTARegistrationClear/VSTARegistrationClear/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace VSTARegistrationClear.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ContainsSearching {
             get {
                 return ((bool)(this["ContainsSearching"]));
